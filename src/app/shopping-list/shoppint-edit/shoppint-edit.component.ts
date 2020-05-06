5b1104bccdebaf3cc9531f39224ac1a05ab5db0e
Shen Qu <qushen024@hotmail.com>
2020-05-06 13:15:02 -0400

    Application set up, create components

---
+++ src/app/shopping-list/shoppint-edit/shoppint-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shoppint-edit',
+  templateUrl: './shoppint-edit.component.html',
+  styleUrls: ['./shoppint-edit.component.css']
+})
+export class ShoppintEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
