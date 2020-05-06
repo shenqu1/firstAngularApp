73648be104971734230be46025dfca882617402b
Shen Qu <qushen024@hotmail.com>
2020-05-06 14:09:33 -0400

    Basic recipe list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppintEditComponent } from './shopping-list/shoppint-edit/shoppint-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,7 @@ import { ShoppintEditComponent } from './shopping-list/shoppint-edit/shoppint-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppintEditComponent
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
