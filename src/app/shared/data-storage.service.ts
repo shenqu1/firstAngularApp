f3ac26b33c3124ae4cba5a86d32569dc7295e4c7
Shen Qu <qushen024@hotmail.com>
2020-05-09 20:49:12 -0400

    http storing recipes - Firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
+import { firebase } from 'src/environments/firebase.environment';
+
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient,
+                private recipeService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put(firebase.url, recipes)
+            .subscribe((response)=>{
+                console.log(response);
+            });
+    }
+}
