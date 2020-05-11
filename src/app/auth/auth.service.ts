0e4dc3f344d4d8b4831e9021935f40ffb45f0f00
Shen Qu <qushen024@hotmail.com>
2020-05-11 19:16:02 -0400

    Environment Variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
-import { firebase } from 'src/environments/firebase.environment';
 import { User } from './auth.model';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 export interface AuthResponseData {
     kind: string;
@@ -24,7 +24,7 @@ export class AuthService {
 
     constructor(private http: HttpClient, private router: Router) {}
     signup(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + firebase.api, {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password,
             returnSecureToken: true
@@ -34,7 +34,7 @@ export class AuthService {
     }
     
     login(email: string, password: string) {
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + firebase.api, {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey, {
             email: email,
             password: password,
             returnSecureToken: true
