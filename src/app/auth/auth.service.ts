6af5caef47aee10a5ffaad6304f8af06b93910a9
Shen Qu <qushen024@hotmail.com>
2020-05-11 13:39:22 -0400

    Auto Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,6 +19,7 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
     user = new BehaviorSubject<User>(null);
+    private tokenExpirationTimer: any;
     
 
     constructor(private http: HttpClient, private router: Router) {}
@@ -55,18 +56,33 @@ export class AuthService {
         const loadUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
         if (loadUser.token) {
             this.user.next(loadUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime()
+            - new Date().getTime();
+            this.autoLogout(expirationDuration);
         }
     }
 
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if (this.tokenExpirationTimer) {
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(()=>{
+            this.logout();
+        }, expirationDuration);
     }
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationData = new Date(new Date().getTime() + expiresIn * 1000);
             const user = new User(email, userId, token, expirationData);
             this.user.next(user);
+            this.autoLogout(expiresIn * 1000);
             localStorage.setItem('userData', JSON.stringify(user));
     }
 
